979998d94bb9036c2438a6755da2d790e0ab3502
sdpython <xavier.dupre@gmail.com>
2014-05-04 19:38:04 +0200

    fix an encoding issue

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# -*- coding; utf-8 -*-
+# -*- coding: utf-8 -*-
 #  Copyright (C) 2013 ---------------
 #  All rights reserved.
 # 
@@ -17,14 +17,14 @@
 #  3. All advertising materials mentioning features or use of this
 #     software must display the following acknowledgment:
 #     "This product includes software developed by
-#      Xavier Dupré <xavier.dupre AT gmail.com>"
+#      Xavier DuprÃ© <xavier.dupre AT gmail.com>"
 # 
 #  4. Redistributions of any form whatsoever must retain the following
 #     acknowledgment:
 #     "This product includes software developed by
-#      Xavier Dupré <xavier.dupre AT gmail.com>."
+#      Xavier DuprÃ© <xavier.dupre AT gmail.com>."
 # 
-#  THIS SOFTWARE IS PROVIDED BY Xavier Dupré ``AS IS'' AND ANY
+#  THIS SOFTWARE IS PROVIDED BY Xavier DuprÃ© ``AS IS'' AND ANY
 #  EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 #  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
 #  PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL Roman V. Kiseliov OR
@@ -98,7 +98,7 @@ if "--verbose" in sys.argv :
 setup(
     name                    = project_var_name,
     version                 = '%s.%s' %(sversion, subversion) if "register" in sys.argv else 'py%s-v%s.%s' % (versionPython, sversion, subversion),
-    author                  = 'Xavier Dupré',
+    author                  = 'Xavier DuprÃ©',
     author_email            = 'xavier.dupre AT gmail.com',
     url                     = "http://www.xavierdupre.fr/app/pyrsslocal/helpsphinx/index.html",
     download_url            = "https://github.com/sdpython/pyrsslocal/",
