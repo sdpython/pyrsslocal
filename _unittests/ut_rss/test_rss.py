a849294325ae74d7b2c87ccf356f3d332f9f2a1a
sdpython <xavier.dupre@gmail.com>
2014-08-20 00:52:48 +0200

    add one blog to a unit test

--- _unittests/ut_rss/test_rss.py
+++ _unittests/ut_rss/test_rss.py
@@ -28,7 +28,7 @@ from pyensae.sql.database_main          import Database
 
 class TestRSS (unittest.TestCase):
     
-    nb_rss_blog = 225
+    nb_rss_blog = 226
     
     def test_rss_from_google (self) :
         fLOG (__file__, self._testMethodName, OutputPrint = __name__ == "__main__")
