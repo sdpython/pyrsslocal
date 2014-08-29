8c1bfc023040f8c73b0579cbe2890bd4f402b7af
sdpython <xavier.dupre@gmail.com>
2014-08-29 12:59:42 +0200

    documentation

--- src/pyrsslocal/helper/externs.py
+++ src/pyrsslocal/helper/externs.py
@@ -1,6 +1,6 @@
 """
 @file
-@brief Shortcuts to functionatilies available in others modules.
+@brief Shortcuts to functionatilies available in other modules.
 """
 
 from pyquickhelper.pandashelper.tblformat import df_to_html
