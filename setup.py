84f05adab337b8690df0a1395b87c98489f90fc7
xavier dupré <xavier.dupre@gmail.com>
2017-08-08 02:00:20 +0200

    licence

--- setup.py
+++ setup.py
@@ -197,6 +197,7 @@ if not r:
         version='%s%s' % (sversion, subversion),
         author='Xavier Dupré',
         author_email='xavier.dupre@gmail.com',
+        license="MIT",
         url="http://www.xavierdupre.fr/app/pyrsslocal/helpsphinx/index.html",
         download_url="https://github.com/sdpython/pyrsslocal/",
         description=DESCRIPTION,
