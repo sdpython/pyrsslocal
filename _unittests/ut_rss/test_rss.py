e3f26426111b5e3d63646e5f3b711d0fcbd37841
xavier dupré <xavier.dupre@gmail.com>
2018-01-04 01:00:05 +0100

    reduce list of blog posts to test

--- _unittests/ut_rss/test_rss.py
+++ _unittests/ut_rss/test_rss.py
@@ -56,7 +56,7 @@ from pyensae.sql.database_main import Database
 
 class TestRSS (unittest.TestCase):
 
-    nb_rss_blog = 238
+    nb_rss_blog = 128
 
     def test_rss_from_google(self):
         fLOG(
