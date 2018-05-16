3d5e71670e1662d35471ba4f7f555e329281177c
xavier dupré <xavier.dupre@gmail.com>
2018-05-17 00:40:57 +0200

    update build and setup

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,35 +1,10 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-import datetime
-import re
 import hachibee_sphinx_theme
+from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(
-            os.path.split(__file__)[0],
-            "..",
-            "..",
-            "..",
-            "..",
-            "pyquickhelper",
-            "src")))
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(
-            os.path.split(__file__)[0],
-            "..",
-            "..",
-            "..",
-            "..",
-            "pyensae",
-            "src")))
-
-from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 
 set_sphinx_variables(__file__, "pyrsslocal", "Xavier Dupré", 2018,
                      "hachibee", hachibee_sphinx_theme.get_html_themes_path(),
