804d2b6b91a5510d1e302fa1c20988a02d397a82
xavier dupré <xavier.dupre@gmail.com>
2021-01-18 13:50:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ if is_local():
         additional_notebook_path=["pyquickhelper", "pyensae", "pymyinstall"],
         requirements=["pyquickhelper", "pyensae", "pymyinstall"],
         additional_local_path=["pyquickhelper", "pyensae", "pymyinstall"],
-        covtoken=("6f97e59c-33f4-4705-a8a6-f529e9604882", "'_UT_37_std' in outfile"))
+        covtoken=("6f97e59c-33f4-4705-a8a6-f529e9604882", "'_UT_39_std' in outfile"))
     if not r and not ({"bdist_msi", "sdist",
                        "bdist_wheel", "publish", "publish_doc", "register",
                        "upload_docs", "bdist_wininst", "build_ext"} & set(sys.argv)):
